8747763966d84b77bbbca8b4ea30c596bb974c83
BhoneHtutKyaw <40414311@napier.ac.uk>
2019-09-04 09:49:28 +0630

    edit department.java

---
+++ src/main/java/com/napier/sem/Department.java
@@ -0,0 +1,89 @@
+package com.napier.sem;
+
+public class Department {
+    /**
+     * Department number
+     */
+    public String dept_no;
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "dept_no=" + dept_no +
+                ", dept_name='" + dept_name + '\'' +
+                ", manager=" + manager +
+                '}';
+    }
+
+
+
+    public String getDept_name() {
+        return dept_name;
+    }
+
+    public void setDept_name(String dept_name) {
+        this.dept_name = dept_name;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    /**
+     * Department name
+     */
+    public String dept_name;
+
+    /**
+     * Employee's manager
+     */
+    public Employee manager;
+}package com.napier.sem;
+
+public class Department {
+    /**
+     * Department number
+     */
+    public String dept_no;
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "dept_no=" + dept_no +
+                ", dept_name='" + dept_name + '\'' +
+                ", manager=" + manager +
+                '}';
+    }
+
+
+
+    public String getDept_name() {
+        return dept_name;
+    }
+
+    public void setDept_name(String dept_name) {
+        this.dept_name = dept_name;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    /**
+     * Department name
+     */
+    public String dept_name;
+
+    /**
+     * Employee's manager
+     */
+    public Employee manager;
+}
